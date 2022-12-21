af2dfe7142fef5232cd821e1809bafd14dad9fdf
Amit Kumar <amrx101@gmail.com>
2022-12-21 12:42:31 +0530

    New Readme, thread safe

--- creational/singleton/README.md
+++ creational/singleton/README.md
@@ -6,3 +6,7 @@ Examples:
 - State Object
 - DataBase Connection
 - Logger
+
+Implementation:
+- Client calls a class method get an instance.
+- There is only 1 instance
--- creational/singleton/__init__.py
+++ creational/singleton/__init__.py
@@ -1,12 +1,43 @@
-class Singleton(object):
-    instance = None
+import threading
 
-    def __init__(self, *args, **kwargs):
-        pass
 
-    @classmethod
-    def Instantiate(cls, *args, **kwargs):
-        if cls.instance is None:
-            cls.instance = cls(*args, **kwargs)
-        return cls.instance
+class SingletonMeta(type):
 
+    _instances = dict()
+
+    def __call__(cls, *args, **kwargs):
+        print("CLS IS", cls)
+        if cls in SingletonMeta._instances:
+            return SingletonMeta._instances[cls]
+
+        with threading.Lock():
+            instance = super(SingletonMeta, cls).__call__(*args, **kwargs)
+            SingletonMeta._instances[cls] = instance
+        print(cls._instances)
+        return SingletonMeta._instances[cls]
+
+
+class ClientA(metaclass=SingletonMeta):
+
+    def __init__(self, a, b):
+        self._a = a
+        self._b = b
+
+
+class ClientB(metaclass=SingletonMeta):
+
+    def __init__(self, a=None, b=None):
+        self._a = a
+        self._b = b
+
+
+if __name__ == "__main__":
+    ca1 = ClientA(10, 11)
+    ca2 = ClientA(11, 12)
+    assert id(ca1) == id(ca2)
+    print(ca1._b, ca2._b)
+
+    cb1 = ClientB()
+    cb2 = ClientB()
+    assert id(cb1) == id(cb2)
+    assert id(ca1) != id(cb1)
